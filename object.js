02ca4073117a7165610e6dff39c180ed2abcd7e8
ninibabs <ninibabalola03@gmail.com>
2021-08-15 17:30:38 +0100

    first commit

---
+++ object.js
@@ -0,0 +1,8 @@
+console.log('i am nini');
+
+
+let obj ={info:"nini",age:9,bloodType: 'B-', school:'Grange school',array1:["nini","babalola","joel",9]}
+
+const {info,age,bloodType,school} =obj
+
+console.log(` my name is ${info} i am ${age} my bloodtype is ${bloodType} i am in ${school} `);
