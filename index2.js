862fb04f7a750464835b38cef33946f1723c1e23
ninibabs <ninibabalola03@gmail.com>
2021-08-21 09:38:15 +0100

    this is nini's first commit

---
+++ index2.js
@@ -0,0 +1,73 @@
+const me = (a,b,c,d,)=>{
+    console.log('my name is ' + a + ' i come from ' + b + 'i am ' + c + ' i like a game called ' + d )
+
+}
+
+me('nini','ekiti',9,'roblox')
+
+
+const crlf = (a,c)=>{
+    console.log('i am ' + a + ' i am ' + c);
+}
+
+crlf("nini",9)
+
+const task = (a,b,c,d,)=>{
+    console.log('my name is ' + a + ' i come from ' + b + 'i am ' + c + ' i like a game called ' + d )
+
+}
+
+task('nini','ekiti',9,'roblox')
+
+const f = (a,b,c,d,)=>{
+    console.log('my name is ' + a + ' i come from ' + b + 'i am ' + c + ' i like a game called ' + d )
+
+}
+
+f('nini','ekiti',9,'roblox')
+
+const we = (a,b,c,d,)=>{
+    console.log('my name is ' + a + ' i come from ' + b + 'i am ' + c + ' i like a game called ' + d )
+
+}
+
+we('nini','ekiti',9,'roblox')
+
+const JS = (a,b,c,d,)=>{
+    console.log('my name is ' + a + ' i come from ' + b + 'i am ' + c + ' i like a game called ' + d )
+
+}
+
+JS('nini','ekiti',9,'roblox')
+
+const store = (a,b,c,d,)=>{
+    console.log('my name is ' + a + ' i come from ' + b + 'i am ' + c + ' i like a game called ' + d )
+
+}
+
+store('nini','ekiti',9,'roblox')
+
+const obk = (a,b,c,d,)=>{
+    console.log('my name is ' + a + ' i come from ' + b + 'i am ' + c + ' i like a game called ' + d )
+
+}
+
+obk('nini','ekiti',9,'roblox')
+
+const cnsle = (a,b,c,d,)=>{
+    console.log('my name is ' + a + ' i come from ' + b + 'i am ' + c + ' i like a game called ' + d )
+
+}
+
+cnsle('nini','ekiti',9,'roblox')
+
+const one = (a,b,c,d,)=>{
+    console.log('my name is ' + a + ' i come from ' + b + 'i am ' + c + ' i like a game called ' + d )
+
+}
+
+one('nini','ekiti',9,'roblox')
+
+
+
+
