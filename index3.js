f0e091eaf37642df226c634fcbc4321932424734
ninibabs <ninibabalola03@gmail.com>
2021-08-21 10:28:12 +0100

    my commitment

---
+++ index3.js
@@ -0,0 +1,17 @@
+const obt =(name,age) => {
+  if(age > 18)  {
+      console.log(`my name is ${name} and i am ${age}`)
+  }else if(age == 9 && name == "seese" ){
+      console.log("you are not " +  name + " and you are not " + age );
+
+  }else if(age == 9 && name=="nini"){
+console.log("my name is Inuola " + "i am the dommest")
+  }else{
+      console.log("the above is not correct")
+  }
+
+  
+} 
+
+
+obt("nini",9)
